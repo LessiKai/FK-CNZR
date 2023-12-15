e7d6d09fb05cf7f5d7f480421e23bb4acdfea724
Pastabuoy <patricknoahmueller@gmail.com>
2023-12-15 15:26:38 +0100

    Working Classification Model v0.5.1

--- cnzr/data.py
+++ cnzr/data.py
@@ -1,9 +1,11 @@
+
+from auxiliary import validate_data
+from training import ModelV1, train_model, test_model
+from sklearn.utils import shuffle
+from pathlib import Path
 import pandas as pd
 import numpy as np
-from pathlib import Path
-from auxiliary import validate_data
 import pickle
-import torch
 
 
 
@@ -15,9 +17,7 @@ CANCER_TYPES = {
     "tcga-kirp": 2,
 }
 
-def data_split(x, y, random_seed):
 
-    return x_train, y_train, x_test, y_test
 
 def load_data():
     data_dir    = Path("./data").resolve()
@@ -27,11 +27,9 @@ def load_data():
         with open(preprocessed_file, "rb") as f:
             x, y = pickle.load(f)
             return x, y
-
+    cancer_dict = dict()
     cancer_dirs = [d for d in data_dir.iterdir() if d.is_dir()]
     print(f"Cancer class dirs: {', '.join([str(d) for d in cancer_dirs])}")
-
-    cancer_dict = dict()
 
     # CANCER LEVEL
     for cancer_dir in cancer_dirs:
@@ -39,54 +37,67 @@ def load_data():
         case_dirs = [d for d in cancer_dir.iterdir() if d.is_dir()]
         print(f"Number of cases: {len(case_dirs)}")
         name_cancer_dir = cancer_dir.name
-        
         # CASE LEVEL
         for case_dir in case_dirs:
             case_file = [f for f in case_dir.iterdir() if f.is_file() and f.suffix == ".tsv"]
             assert len(case_file) == 1
-            case_file = case_file[0]
-            
-            case_data = pd.read_csv(case_file, sep="\t", skiprows = 1)
+            case_data = pd.read_csv(case_file[0], sep="\t", skiprows = 1)
             case_data = case_data.drop(labels = case_data.columns[[0,3,4,5,7,8]], axis="columns")
 
             #entferne NA Zeilen und dann alle anderen Gene Type Zeilen
             case_data = case_data[case_data['gene_type'].notna()]
             case_data = case_data[case_data['gene_type'].str.contains('protein_coding')]
             case_data = case_data.drop(labels = case_data.columns[[1]], axis="columns")
-
             x = case_data["tpm_unstranded"].to_numpy()
             y = np.full(x.shape, cancer_dir.name)
             cancer_dict[case_dir.name] = {
                 "x": x,
                 "y": y
             }
-
     n_cases = len(cancer_dict)
     n_genes = cancer_dict[next(iter(cancer_dict))]["x"].shape[0]
-
     x = np.zeros((n_cases, n_genes))
     y = np.zeros(n_cases)
 
     for i, (case_name, case_data) in enumerate(cancer_dict.items()):
         x[i] = case_data["x"]
         y[i] = CANCER_TYPES[case_data["y"].astype(str)[0]]
-
     print(f"Length of Cancer Dictionary: {len(cancer_dict)}. Dataset Ready. \n")
-
     # save dict
     with open(data_dir / "preprocessed.pkl", "wb") as f:
         pickle.dump((x, y), f)
-
     return x, y
 
 
+
 if __name__ == "__main__":
-    #validate_data()
+
+    ratio_train_test = 0.7
+
+    # validate_data()
     x, y = load_data()
-    print(x.shape)
-    print(y.shape)
 
-    random_seed = 0
-    #x_train, y_train, x_test, y_test = data_split(x, y, random_seed)  
+    ## Bring y into the right form ([y1 = 1, y2 = 0, y3 =0] z.B.) 
+    y_t = np.zeros((len(y), len(CANCER_TYPES)))
+    for idx, value in enumerate(y):
+        y_t[idx][int(value)] = 1
+    y = y_t
+    del y_t
+    print(f"Shape y transformed = {y.shape}")
+
+    #Train Test Split
+    random_seed = 42
+    x, y    = shuffle(x, y, random_state=0)
+    idx     = int(ratio_train_test*len(x))
+    x_train = x[range(1, idx)]
+    y_train = y[range(1, idx)]
+    x_test  = x[range(idx+1, len(x))]
+    y_test  = y[range(idx+1, len(y))]
+   
+    NN          = ModelV1(np.shape(x)[1], len(CANCER_TYPES) )
+    Classifier  = train_model(x_train, y_train, NN, n_classes= len(CANCER_TYPES))
+    test_model(x_test, y_test, Classifier)
 
 
+
+ 
--- cnzr/training.py
+++ cnzr/training.py
@@ -1,4 +1,9 @@
+
 import torch
+import torch.optim as optim
+import torch.utils.data
+import numpy as np
+from colorama import Fore, Back, Style
 from torch import nn
 
 
@@ -8,16 +13,88 @@ class ModelV1(nn.Module):
         super().__init__()
 
         self.network = nn.Sequential(
-            nn.Linear(in_features, 64),
+            nn.Linear(in_features, 128),
+            nn.Tanh(),
+            nn.Linear(128, 64),
             nn.Tanh(),
             nn.Linear(64, 32),
             nn.Tanh(),
             nn.Linear(32, out_features),
         )
 
-
     def forward(self, x):
         x = self.network(x)
         x = torch.softmax(x, dim=1)
         return x
 
+
+
+def train_model(x, y, model, n_classes, print_debug = True):
+    # Controll Variables for the NN
+    epochs              = 100
+    elements_per_batch  = 20
+    learning_rate       = 0.00005
+    if(elements_per_batch>len(x)/2): elements_per_batch = len(x)
+
+    # Data Preprocessing
+    ## Sample X-y into Batches
+    x = x[0:len(x) -len(x) % elements_per_batch]        # Arraytrunkierung des Rests (Vereinfacht Algorithmus)
+    y = y[0:len(y) -len(y) % elements_per_batch]
+    n_batches = int(len(x) / elements_per_batch)
+    if(print_debug == True): print(f"Length X = {len(x)}")
+
+    ## TENSOR Konstruktion für NN
+    x_batches = torch.tensor(np.split(x             , n_batches), dtype=torch.float32)
+    y_batches = torch.tensor(np.split(y , n_batches))
+    
+    # Model Training
+    print(f"\n→ Initiating Modeltraining. Total Epochs: {epochs}")
+    
+    epoch_print = 0
+    loss_fn = nn.CrossEntropyLoss() # Setup Loss Function
+
+    torch.manual_seed(42)
+    for epoch in range(epochs):  
+        if epoch == 0 or epochs < 10: 
+            print_command = True
+        else: 
+            if (epoch % (epochs/20) == 0): 
+                print_command = True
+            else:
+                print_command = False
+        if print_command: print(f"{"↺": ^2} | Epoch: {epoch: ^4}     Advanced {int(epoch/epochs*100): ^4}%     ", end='')
+        
+        model.train()
+        for idx, x_batch in enumerate(x_batches):
+            y_batch = y_batches[idx]
+            y_pred  = model(x_batch)
+            optim   = torch.optim.Adam(model.parameters(), lr=learning_rate)     # Setup optimizer
+            loss    = loss_fn(y_pred, y_batch)
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+        if print_command: 
+            y_pred = model(x_batches[0])
+            print(f"Batch[0] avg. loss: {loss_fn(y_pred, y_batches[0]):.4f}     ")
+    
+        
+    print("Model has been Trained 💪")
+    return model
+    
+
+
+def test_model(x, y, model):
+    model.eval()
+    y_pred = model(torch.tensor(x[0:10], dtype=torch.float32))
+    print(f"{"y": ^15} {"y_pred": ^15}")
+    for i,data in enumerate(y_pred):
+        if str(y[0:10][i].tolist()) == str(torch.round(y_pred[i]).tolist()):
+            print(f"{Fore.GREEN}{str(y[0:10][i].tolist()): ^13} {str(torch.round(y_pred[i]).tolist()): ^13}")
+        else: 
+            print(f"{Fore.RED}{str(y[0:10][i].tolist()): ^13} {str(torch.round(y_pred[i]).tolist()): ^13}", end="")
+            print(Fore.RESET)
+
+    # 1-1, 1-0, 0-1, 0-0
+    # Right Trues, Right False, Wrong Trues, 
+
+    return
